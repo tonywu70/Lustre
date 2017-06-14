abd6fa15168ce8215d972476802d337cfdffad50
Vijendra <Solliancedev@outlook.com>
2017-06-14 14:45:26 +0530

    fix

--- beegfsscripts/install_beegfs.sh
+++ beegfsscripts/install_beegfs.sh
@@ -120,15 +120,15 @@ EOF
         
         mdadm /dev/$raidDevice
 
-        if [ "$filesystem" == "xfs" ]; then
+        #if [ "$filesystem" == "xfs" ]; then
             #mkfs -t $filesystem /dev/$raidDevice
             #echo "/dev/$raidDevice $mountPoint $filesystem rw,noatime,attr2,inode64,nobarrier,sunit=1024,swidth=4096,nofail 0 2" >> /etc/fstab
-        else
+        #else
             #mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype /dev/$raidDevice
             sleep 5
             #tune2fs -o user_xattr /dev/$raidDevice
             #echo "/dev/$raidDevice $mountPoint $filesystem noatime,nodiratime,nobarrier,nofail 0 2" >> /etc/fstab
-        fi
+        #fi
         
         sleep 10
         
