c13a1c967bc3380e825440cb5847f5d1cff2ee89
Vijendra <Solliancedev@outlook.com>
2017-06-15 13:20:06 +0530

    changes

--- Scripts/install_azureCLI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-        yum -y install nodejs
-
-        [[ -z "$HOME" || ! -d "$HOME" ]] && { echo 'fixing $HOME'; HOME=/root; }
-        export HOme
-        npm install -g azure-cli
-        azure telemetry --disable
