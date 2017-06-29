bc3e253bfee0fac5b1bd30301a81ac1035b3fe25
azmigproject <azmigproject@outlook.com>
2017-06-29 14:21:20 +0530

    Updated shell script

--- scripts/install_lustre_client.sh
+++ scripts/install_lustre_client.sh
@@ -38,103 +38,6 @@ install_pkgs()
     yum -y install zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget python-pip openmpi openmpi-devel automake autoconf
 }
 
-# Partitions all data disks attached to the VM and creates
-# a RAID-0 volume with them.
-#
-setup_data_disks()
-{
-    mountPoint="$1"
-    filesystem="$2"
-    devices="$3"
-    raidDevice="$4"
-    createdPartitions=""
-
-    # Loop through and partition disks until not found
-    for disk in $devices; do
-        fdisk -l /dev/$disk || break
-        fdisk /dev/$disk << EOF
-n
-p
-1
-
-
-t
-fd
-w
-EOF
-        createdPartitions="$createdPartitions /dev/${disk}1"
-    done
-    
-    sleep 10
-
-    # Create RAID-0/RAID-5 volume
-    if [ -n "$createdPartitions" ]; then
-        devices=`echo $createdPartitions | wc -w`
-        mdadm --create /dev/$raidDevice --level 0 --raid-devices $devices $createdPartitions
-        
-        sleep 10
-        
-        mdadm /dev/$raidDevice
-
-        if [ "$filesystem" == "xfs" ]; then
-            mkfs -t $filesystem /dev/$raidDevice
-            echo "/dev/$raidDevice $mountPoint $filesystem rw,noatime,attr2,inode64,nobarrier,sunit=1024,swidth=4096,nofail 0 2" >> /etc/fstab
-        else
-            mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype /dev/$raidDevice
-            sleep 5
-            tune2fs -o user_xattr /dev/$raidDevice
-            echo "/dev/$raidDevice $mountPoint $filesystem noatime,nodiratime,nobarrier,nofail 0 2" >> /etc/fstab
-        fi
-        
-        sleep 10
-        
-        mount /dev/$raidDevice
-    fi
-}
-
-setup_disks()
-{      
-    # Dump the current disk config for debugging
-    fdisk -l
-    
-    # Dump the scsi config
-    lsscsi
-    
-    # Get the root/OS disk so we know which device it uses and can ignore it later
-    rootDevice=`mount | grep "on / type" | awk '{print $1}' | sed 's/[0-9]//g'`
-    
-    # Get the TMP disk so we know which device and can ignore it later
-    tmpDevice=`mount | grep "on /mnt/resource type" | awk '{print $1}' | sed 's/[0-9]//g'`
-
-    # Get the metadata and storage disk sizes from fdisk, we ignore the disks above
-    #metadataDiskSize=`fdisk -l | grep '^Disk /dev/' | grep -v $rootDevice | grep -v $tmpDevice | awk '{print $3}' | sort -n -r | tail -1`
-    storageDiskSize=`fdisk -l | grep '^Disk /dev/' | grep -v $rootDevice | grep -v $tmpDevice | awk '{print $3}' | sort -n | tail -1`    
-	
-		# Compute number of disks
-		nbDisks=`fdisk -l | grep '^Disk /dev/' | grep -v $rootDevice | grep -v $tmpDevice | wc -l`
-		echo "nbDisks=$nbDisks"
-
-		storageDevices="`fdisk -l | grep '^Disk /dev/' | grep $storageDiskSize | awk '{print $2}' | awk -F: '{print $1}' | sort | tail -$nbStorageDisks | tr '\n' ' ' | sed 's|/dev/||g'`"
-   
-		mkdir -p $LUSTRE_STORAGE
-		setup_data_disks $LUSTRE_STORAGE "xfs" "$storageDevices" "md0"	
-    mount -a
-}
-setup_raid()
-{
-	#Update system and install mdadm for managing RAID
-	yum clean all && yum update
-	yum install mdadm -y
-
-	#Verify attached data disks
-	ls -l /dev | grep sd
-
-	#Examine data disks
-	mdadm --examine /dev/sd[c-l]
-
-	#Create RAID md device
-	mdadm -C /dev/md0 -l raid0 -n 10 /dev/sd[c-l]
-}
 setup_user()
 {
     mkdir -p $SHARE_HOME
@@ -198,7 +101,6 @@ if [ -e "$SETUP_L" ]; then
     echo "We're already configured, exiting..."
     exit 0
 fi
-touch /root/lustrestart.setup
 mkdir /mnt/lustre
 mount -t lustre $MGMT_HOSTNAME@tcp:/LustreFS /mnt/lustre
 touch /root/lustre.setup
@@ -229,7 +131,7 @@ install_pkgs
 setup_user
 install_lustre_repo
 install_lustre
-#setup_lustrecron
+setup_lustrecron
 #download_lis
 #install_lis_in_cron
 
